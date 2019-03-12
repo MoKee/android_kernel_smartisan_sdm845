6c12433117b1f9f089c8f102fadf7a468b37d5c0
XiNGRZ <chenxingyu92@gmail.com>
2019-03-12 13:07:27 +0800

    SMARTISAN: Import changes - USB
    Change-Id: Icee50b1980864976e70be6f96b7751250adebed9

--- drivers/usb/pd/usbpd.h
+++ drivers/usb/pd/usbpd.h
@@ -56,6 +56,21 @@ enum pd_spec_rev {
 	USBPD_REV_30 = 2,
 };
 
+#ifdef CONFIG_VENDOR_SMARTISAN
+struct usbpd_attr {
+	int		cc_orientation;
+	int		pwr_role;
+	int		alt_mode;
+	int		typec_mode;
+};
+
+enum tusb544_alt_mode {
+	TUSB544_ALTERNATE_MODE_NONE,
+	TUSB544_ALTERNATE_MODE_DP,
+	TUSB544_ALTERNATE_MODE_CUSTOM,
+};
+#endif
+
 /* enable msg and signal to be received by phy */
 #define FRAME_FILTER_EN_SOP		BIT(0)
 #define FRAME_FILTER_EN_HARD_RESET	BIT(5)
