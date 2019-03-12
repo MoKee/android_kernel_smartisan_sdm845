ee0c17a43f37db9b4d61a9649329a653537ca7a9
XiNGRZ <chenxingyu92@gmail.com>
2019-03-12 11:01:06 +0800

    SMARTISAN: Import changes - DRM
    Change-Id: Iaa4c1c29becd85623f779f76040462e38892b7c2

--- drivers/gpu/drm/msm/dp/dp_link.h
+++ drivers/gpu/drm/msm/dp/dp_link.h
@@ -109,6 +109,9 @@ static inline char *dp_link_get_test_name(u32 test_requested)
 struct dp_link {
 	u32 sink_request;
 	u32 test_response;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	bool psm_enabled;
+#endif
 
 	struct dp_link_sink_count sink_count;
 	struct dp_link_test_video test_video;
