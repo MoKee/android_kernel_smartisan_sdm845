8b9530228ffb3402fe36b88cc0f94345bf93e37d
XiNGRZ <chenxingyu92@gmail.com>
2019-03-12 13:17:41 +0800

    SMARTISAN: Import changes - device mapper
    Change-Id: I905b4b6de45357fae06223b7ae3b9c6550094675

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -880,6 +880,9 @@ static sector_t max_io_len(sector_t sector, struct dm_target *ti)
 {
 	sector_t len = max_io_len_target_boundary(sector, ti);
 	sector_t offset, max_len;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	sector_t optimal_len;
+#endif
 
 	/*
 	 * Does the target need to split even further?
@@ -894,6 +897,12 @@ static sector_t max_io_len(sector_t sector, struct dm_target *ti)
 
 		if (len > max_len)
 			len = max_len;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	} else if (ti->asymmetric_chunk_supported) {
+		offset = dm_target_offset(ti, sector);
+		ti->type->io_calculate(ti, offset, &optimal_len);
+		len = optimal_len;
+#endif
 	}
 
 	return len;
